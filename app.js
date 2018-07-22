ff9e5bb15c8cf1f3cd4d10f0a8148b17cc5887a5
sberlin <sberlin@users.noreply.github.com>
2018-07-22 16:32:58 +0200

    Minimum viable draft of a debuggable motor controller

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
+const readline = require('readline');
 const Motor = require('./src/motor');
+
+const motor = new Motor({pwm: 4, fwd: 17, rev: 18});
+
+console.log('Forward/Reverse/Stop (f/r/s): ');
+
+const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+});
+rl.prompt();
+
+rl.on('line', (line) => {
+    if (line.trim()) {
+        let input = line.trim().toLowerCase().split(' ');
+        if (input[0] === 'f') {
+            console.log(`Forward with speed ${input[1]} for ${input[2]} seconds`);
+            motor.forward(input[1], input[2]);
+        }
+        if (input[0] === 'r') {
+            console.log(`Reverse with speed ${input[1]} for ${input[2]} seconds`);
+            motor.reverse(input[1], input[2]);
+        }
+        if (input[0] === 's') {
+            console.log('Stop');
+            motor.stop();
+        }
+    }
+});
 
--- src/motor.js
+++ src/motor.js
@@ -1,6 +1,44 @@
 const Gpio = require('pigpio').Gpio;
 
 class Motor {
+    constructor (pins = {}) {
+        this.LOW = 0;
+        this.HIGH = 255;
+        this.pins = {};
+        for (let pin in pins) {
+            if (pins.hasOwnProperty(pin)) {
+                this.pins[pin] = new Gpio(
+                    pins[pin],
+                    {mode: Gpio.OUTPUT}
+                );
+            }
+        }
+    }
+    _start (speed) {
+        this.pins.pwm.pwmWrite(speed || this.HIGH);
+    }
+    stop () {
+        this.pins.pwm.pwmWrite(this.LOW);
+    }
+    forward (speed, seconds) {
+        this._timeout(seconds);
+        this.pins.fwd.pwmWrite(this.HIGH);
+        this.pins.rev.pwmWrite(this.LOW);
+        this._start(speed);
+    }
+    reverse (speed, seconds) {
+        this._timeout(seconds);
+        this.pins.fwd.pwmWrite(this.LOW);
+        this.pins.rev.pwmWrite(this.HIGH);
+        this._start(speed);
+    }
+    _timeout (seconds = 0) {
+        if (seconds) {
+            setTimeout(() => {
+                this.stop();
+            }, seconds * 1000);
+        }
+    }
 }
 
 module.exports = Motor;

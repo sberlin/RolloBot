3b82f4b0dc59b9178f958ae089b8b1272aac7971
sberlin <sberlin@users.noreply.github.com>
2018-08-30 14:26:42 +0200

    Comply with Neonious layout but keep Node compatibility

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const Motor = require('./src/motor');
-const models = require('./src/models');
 
 if (process && process.env.NODE_ENV === "DEBUG") {
+    const readline = require('readline');
+    const Motor = require('./src/motor');
+    const models = require('./src/models');
+
     console.log("Running on Node.js in debug mode");
     console.log("Use basic CLI for debugging");
 
     // mock pins
     const motor = new Motor({pwm: 4, fwd: 17, rev: 18}, models.MOCK);
 
-    const readline = require('readline');
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -37,9 +38,5 @@ if (process && process.env.NODE_ENV === "DEBUG") {
 } else {
     console.log("Running in production mode");
 
-    // use these pins on Neonious One as default
-    const motor = new Motor({pwm: 7, fwd: 8, rev: 9}, models.NEONIOUS_ONE);
-
-    // TODO
-    motor.forward(1, 10);
+    require('./src/index.js');
 }
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const Motor = require('./motor');
+const models = require('./models');
+
+// use these pins on Neonious One as default
+const motor = new Motor({pwm: 7, fwd: 8, rev: 9}, models.NEONIOUS_ONE);
+
+// TODO
+motor.forward(1, 10);
